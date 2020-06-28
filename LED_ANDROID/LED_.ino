73d4c5600c30b4b112d3943ba6c738c73344c851
Arun_Kumar <45923721+Akumar7042@users.noreply.github.com>
2020-06-28 06:13:29 -0400

    Create LED_.ino
    Ardiuno Code

---
+++ LED_ANDROID/LED_.ino
@@ -0,0 +1,21 @@
+char Incoming_value = 0;
+                
+void setup() 
+{
+  Serial.begin(9600);         
+  pinMode(8, OUTPUT); //Led Connet At Port 8       
+}
+
+void loop()
+{
+  if(Serial.available() > 0)  
+  {
+    Incoming_value = Serial.read();      
+    Serial.print(Incoming_value);        
+    Serial.print("\n");        
+    if(Incoming_value == '1')             
+      digitalWrite(8, HIGH);  
+    else if(Incoming_value == '0')       
+      digitalWrite(8, LOW);   
+  }                            
+} 
